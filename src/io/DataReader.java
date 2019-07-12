63c290aad8cdc638030e846243fb52f2061d26d0
wsadrak <w.sadrak@gmail.com>
2019-07-12 12:13:58 +0200

    created class to interact wit user

---
+++ src/io/DataReader.java
@@ -0,0 +1,24 @@
+package io;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class DataReader {
+	private Scanner scanner = new Scanner(System.in);
+	
+	public int getInt() {
+		try {
+			return Integer.parseInt(readInput());
+		} catch (NumberFormatException e) {
+			throw new NoSuchElementException("It's not a number! ");
+		} 
+	}
+	
+	public String readInput() {
+		return scanner.nextLine();
+	}
+
+	public void close() {
+		scanner.close();
+	}
+
+}
